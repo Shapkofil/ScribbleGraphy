c18ee36b3edaefa78f889d5c36c665ff139173c2
Shapkofil <kiko_ki@mail.bg>
2019-05-10 00:35:52 +0300

    Revert From DownTown

--- C#/WorkingControls/FreeWritingWindow.cs
+++ C#/WorkingControls/FreeWritingWindow.cs
@@ -255,7 +255,31 @@ namespace WorkingControls
             else ThreadHelperClass.SetText(this, simbolLabel, "Recognizing.......");
 =======
             Program.modelReader.GiveTask(imgindex);
+<<<<<<< HEAD
 >>>>>>> 23e017fecc78ea1afe501226aaacae23f442804f
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        }
+        public void OnSoftmaxFire(object soruce, cmdEventArgs e)
+        {
+            if (e.prediction != null)
+            {
+                ThreadHelperClass.SetText(this, simbolLabel, "acc= " + e.acc + "%");
+                if (activity == 0) PredictionDisplay.Image =
+                       VisualExamples.img_dic[currentWritingSystem][Int32.Parse(e.prediction)];
+                resultCharacter = e.prediction;
+            }
+            else ThreadHelperClass.SetText(this, simbolLabel, "Recognizing.......");
+=======
+>>>>>>> parent of 41be0232... Fixed Template writing
+=======
+>>>>>>> parent of 41be0232... Fixed Template writing
+=======
+            Debug.Print(Properties.Settings.Default.currentWS + " " + imgindex);
+>>>>>>> parent of bc6ac773... Merge branch 'development' of https://github.com/Shapkofil/ScribbleGraphy into development
+>>>>>>> parent of fbb93619... StableWithOutGhoustCode
         }
 
         //Events for Buttons
